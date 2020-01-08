ae63aad3f83c905a8a93a0257cfcc1760f1b0eb0
Fletcher Deal <fletcherdeal@gmail.com>
2020-01-07 23:51:08 -0500

    created react app, added spotify auth

---
+++ mixtape/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={()=>window.location='http://localhost:8888/login'}>Sign in with spotify</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
